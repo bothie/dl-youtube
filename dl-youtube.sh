ae7b27958e7f72bf870807931636069141f227df
Bodo Thiesen <bothie@gmx.de>
2012-06-10 03:49:41 +0200

    Moved urldecode to a separate script (outside of this git repo).

--- dl-youtube.sh
+++ dl-youtube.sh
@@ -67,37 +67,6 @@ if test $num_args -gt   100; then num_digits=3; fi
 if test $num_args -gt  1000; then num_digits=4; fi
 if test $num_args -gt 10000; then num_digits=5; fi
 
-urldecode () {
-	if test -n "$(type -p httpdecode)"
-	then
-		(
-			while read url
-			do
-				httpdecode --decode "$url"
-			done
-		)
-	else
-		sed \
-			-e 's/+/ /g' \
-			-e "s/%23/#/g" \
-			-e 's_%26_\&_g' \
-			-e "s/%27/'/g" \
-			-e "s/%28/(/g" \
-			-e "s/%29/)/g" \
-			-e 's_%2C_,_g' \
-			-e 's_%2F_/_g' \
-			-e 's/%3A/:/g' \
-			-e 's/%3D/=/g' \
-			-e 's/%3F/?/g' \
-			-e 's/%5B/[/g' \
-			-e 's/%5D/]/g' \
-			-e 's/%25/%/g' \
-			-e 's/%C3%BC/ü/g' \
-			-e 's/%C3%B6/ö/g' \
-	
-	fi
-}
-
 urlencode () {
 	echo ">>> urlencode" >&2
 	if test -n "$(type -p httpencode)"
